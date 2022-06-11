30b8e1ba13216df2feefb1a164c3ec2ce74bdc53
Sharan Phadke <phadkesharan@gmail.com>
2022-06-11 11:39:13 +0530

    NEW: add cart, diagram images

--- src/components/topContent.jsx
+++ src/components/topContent.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import image from "../assets/image.png"
+import cart from "../assets/cart.png"
 
 export default function TopContent() {
     return (
         <div className="fs-1 px-5 py-2 text-center" style={{ height: "1000px", backgroundColor: "rgb(250, 240, 234)", color: "black" }}>
-            Our mission is to digitalise physical market and bridge the gap betweem physical and digital market
+            <div>
+                Our mission is to digitalise physical market and bridge the gap betweem physical and digital market
+            </div>
+
+            <div className="container my-4">
+                <img src={image} alt="" />
+            </div>
+
+            <div>
+                We Present to you ThirdWheel
+            </div>
+
+            <div className="container my-4">
+                <img src={cart} alt=""  height="400px"/>
+            </div>
         </div>
     )
 }
